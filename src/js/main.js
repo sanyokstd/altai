0e4db26f32553786c5321557e31a757afcd73a1c
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-11-23 11:20:27 +0200

    1
    1

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,15 @@ $(function(){
 
 
     $('.burger').click(function(){
-    	$(this).toggleClass('active')
+      $(this).toggleClass('active')
+      $('.mob-menu').fadeToggle()
+    })
+
+    $('.mob-menu .level1').click(function(){
+      $('.mob-menu .level2 a').fadeIn()
+    })
+
+    $('.level2__close').click(function(){
+      $('.mob-menu .level2').fadeOut()
     })
 })
